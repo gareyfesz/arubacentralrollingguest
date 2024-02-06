bc65880ba3735838dffffd890b262ba12ae7f308
gareyfesz <garey@gareyfesz.com>
2024-02-06 09:16:14 -0500

    added central_info

---
+++ central_info.py
@@ -0,0 +1,8 @@
+central_info = {
+        "username": "central_username_non_sso",
+        "password": "central_password",
+        "client_id": "client_id",
+        "client_secret": "client_secret",
+        "customer_id": "customer_id",
+        "base_url": "base_url"
+    }
